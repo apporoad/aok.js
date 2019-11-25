dd2620c429fee2fc925a683154ce897e43a0daba
apporoad <apporoadina@163.com>
2019-11-25 09:57:18 +0800

    port

--- docs/dockerRun.sh
+++ docs/dockerRun.sh
@@ -3,7 +3,7 @@
 if   [[ $aok_type = "git" ]] || [[ $aok_type = 'zip' ]]; 
 then 
 echo 'node /aok/bin.js '$aok_path' -w /workspace/aok --type '$aok_type' '$aok_param'  -d -p 1154'
-node /aok/bin.js $aok_path -w /workspace/aok --type $aok_type $aok_param  -d
+node /aok/bin.js $aok_path -w /workspace/aok --type $aok_type $aok_param  -d -p 1154
 else 
 echo 'node /aok/bin.js  /api -s /static -p 1154'
 node /aok/bin.js  /api -s /static -p 1154 
